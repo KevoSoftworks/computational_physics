4a84b091b9f504d4324c56e424d453007bffd761
Kevin Vonk <KevoSoftworks@gmail.com>
2020-03-09 11:46:52 +0100

    Change Solver.iterate to become a generator, Add fullsolve, Add finished Part 1

--- 2_magnetism/magnetism.py
+++ 2_magnetism/magnetism.py
@@ -15,7 +15,7 @@ class Lattice:
 
 	@property
 	def N(self):
-		return L**2
+		return self.L**2
 
 	@property
 	def Z(self):
@@ -70,8 +70,9 @@ class Solver:
 	def __init__(self, lattice):
 		self.lattice = lattice
 
-	def iterate(self, iterations):
-		for _ in range(iterations):
+	def iterate(self, iterations = -1):
+		i = 0
+		while i < iterations or iterations == -1:
 			tmp = copy.deepcopy(self.lattice).flip()
 			dE = tmp.delta_e(self.lattice)
 
@@ -80,4 +81,37 @@ class Solver:
 				self.lattice.E += dE
 				#self.lattice.M is a function and needn't be updated
 			
+			i += 1
+
 			yield self.lattice
+	
+	# Solve using large steps L**2
+	def fullsolve(self, iterations, ret_val = ("<m>")):
+		gen = self.iterate()
+		M = []
+		ret = {}
+
+		for _ in range(iterations):
+			for _ in range(self.lattice.N):
+				lat = next(gen)
+			
+			M.append(lat.M)
+		
+		M = np.array(M)
+		
+		if "<m>" in ret_val:
+			ret["<m>"] = 1/iterations * np.sum(M) / self.lattice.N
+
+		if "M" in ret_val:
+			ret["M"] = M
+
+		if "<M>" in ret_val:
+			ret["<M>"] = 1/iterations * np.sum(M)
+
+		if "chi" in ret_val:
+			exp_M = 1/iterations * np.sum(M)
+			exp_M2 = 1/iterations * np.sum(M**2)
+
+			ret["chi"] = self.lattice.beta * (exp_M2 - exp_M**2)
+
+		return ret
