9850b54cf12fbb65a9fc5a8cbfc904e85f5f6995
Kevin Vonk <kevin@kevosoftworks.com>
2021-03-15 13:29:03 +0100

    Fix accuracy issues

--- 3_wave_propagation/wave_propagation.py
+++ 3_wave_propagation/wave_propagation.py
@@ -9,7 +9,7 @@ class Matrix:
 		self.midpoint = False
 	
 	def eta(self, y):
-		if y < self.potential.start or y > self.potential.end or not self.midpoint:
+		if y < self.potential.start or y >= self.potential.end or not self.midpoint:
 			return np.sqrt(self.potential[y] - self.lambda_ + 0j)
 
 		return np.sqrt(self.potential[y + (self.potential.step / 2.)] - self.lambda_ + 0j)
@@ -31,46 +31,51 @@ class Matrix:
 			np.exp(-2 * np.sum(self.potential.step * np.sqrt(dE)))
 
 class TransferMatrix(Matrix):
+	DTYPE = np.cdouble
+
 	@cached_property
 	def M(self):
 		p = lambda prev, cur: 1/(2*prev) * np.array([ \
 			[prev + cur, prev - cur], \
 			[prev - cur, prev + cur] \
-		])
+		], dtype=TransferMatrix.DTYPE)
 
 		q = lambda cur: np.array([ \
 			[np.exp(cur * self.potential.step), 0], \
 			[0, np.exp(-cur * self.potential.step)] \
-		])
+		], dtype=TransferMatrix.DTYPE)
 
-		product = np.array([[1, 0], [0, 1]])
+		product = np.array([[1, 0], [0, 1]], dtype=TransferMatrix.DTYPE)
 
-		for y in self.potential.y[1:-2]:
+		for y in self.potential.y[0:-1]:
 			eta_prev = self.eta(y - self.potential.step)
 			eta_cur = self.eta(y)
 
 			product = product @ p(eta_prev, eta_cur) @ q(eta_cur)
 
-		eta_prev = self.eta(self.potential.end - self.potential.step)
+		eta_prev = eta_cur
 		eta_cur = self.eta(self.potential.end)
 				
 		return np.array([ \
-			[np.exp(-self.eta(self.potential.start - self.potential.step) * (self.potential.start)), 0], \
-			[0, np.exp(self.eta(self.potential.start - self.potential.step) * (self.potential.start))] \
-		]) @ product \
-		@ p(eta_prev, eta_cur) @ np.array([ \
+			[np.exp(-self.eta(self.potential.start - self.potential.step) * self.potential.start), 0], \
+			[0, np.exp(self.eta(self.potential.start - self.potential.step) * self.potential.start)] \
+		], dtype=TransferMatrix.DTYPE) \
+		@ product \
+		@ p(eta_prev, eta_cur) \
+		@ np.array([ \
 			[np.exp(self.eta(self.potential.end) * self.potential.end), 0], \
 			[0, np.exp(-self.eta(self.potential.end) * self.potential.end)] \
-		])
+		], dtype=TransferMatrix.DTYPE)
 	
 	def flush(self):
 		del self.M
 	
 	def transmission(self, dir="right"):
+		# The definitions of right and left are reversed, unfortunately.
 		if dir == "right":
 			return 1/(np.abs(self.M[0, 0])**2)
 		
-		return np.abs(self.M[1, 1] - (self.M[1, 0] * self.M[0, 1]) / self.M[0, 0]) ** 2
+		return np.abs(self.M[1, 1] - self.M[1, 0] * self.M[0, 1] / self.M[0, 0]) ** 2
 	
 	def reflection(self, *args):
 		return np.abs(self.M[1, 0])**2 / np.abs(self.M[0, 0])**2
@@ -98,7 +103,7 @@ class ScatterMatrix(Matrix):
 		
 		m = []
 
-		for y in self.potential.y[1:-1]:
+		for y in self.potential.y:
 			prev = self.eta(y - self.potential.step)
 			cur = self.eta(y)
 
@@ -113,7 +118,8 @@ class ScatterMatrix(Matrix):
 		del self.S
 
 	def transmission(self, dir="right"):
-		prefactor = np.abs(self.eta(self.potential.y[-1])/self.eta(self.potential.y[0]))
+		prefactor = np.abs(self.eta(self.potential.end + self.potential.step) \
+			/ self.eta(self.potential.start - self.potential.step))
 
 		if dir == "right":
 			return prefactor * np.abs(self.S[1, 0])**2
@@ -133,9 +139,6 @@ class Potential:
 		self.pot = np.zeros(self.len)
 		self.y, self.step = np.linspace(self.start, self.end, self.len, retstep=True)
 
-		self.y = np.insert(self.y, 0, self.start - self.step)
-		self.y = np.append(self.y, self.end + self.step)
-
 		if type(expr) == int or type(expr) == float:
 			self.pot = expr * np.ones(self.len)
 		elif callable(expr):
@@ -149,7 +152,7 @@ class Potential:
 		self.outside_range = outside_range
 
 	def __getitem__(self, y):
-		if y <= self.start:
+		if y < self.start:
 			return self.outside_range[0](y)
 		elif y >= self.end:
 			return self.outside_range[1](y)
